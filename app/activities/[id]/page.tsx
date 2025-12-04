702ef973ef9a22fb997a8890a6b6c5d6329d698b
nuix09 <136358529+nuix09@users.noreply.github.com>
2025-12-04 19:54:49 +0530

    Added a coming soon text to activities page

--- app/activities/[id]/page.tsx
+++ app/activities/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { activities } from "../../../data/activities";
 import { notFound } from "next/navigation";
+import TextPressure from "@/components/ui/textPressure";
+import DecryptedText from "@/components/ui/decryptedText";
 
 export default async function ActivityDetailsPage({
   params,
@@ -12,16 +14,43 @@ export default async function ActivityDetailsPage({
   if (!activity) return notFound();
 
   return (
-    <div className="max-w-3xl mx-auto py-10 px-4">
+    <div className="max-w-full mx-auto">
       <img
         src={activity.image}
         alt={activity.title}
-        className="w-full h-80 object-cover rounded-xl mb-6"
+        className="w-full h-80 object-cover mb-6"
       />
 
-      <h1 className="text-3xl font-bold mb-2">{activity.title}</h1>
+      {/* <h1 className="text-3xl font-bold mb-2">{activity.title}</h1>
       <p className="text-lg text-gray-700 mb-4">{activity.shortInfo}</p>
-      <p className="text-gray-600">{activity.details}</p>
+      <p className="text-gray-600">{activity.details}</p> */}
+
+      <div style={{ position: "relative", height: "300px" }}>
+        <TextPressure
+          text="Coming Soon!"
+          flex={true}
+          alpha={false}
+          stroke={false}
+          width={true}
+          weight={true}
+          italic={true}
+          textColor="#005246"
+          strokeColor="#ff0000"
+          minFontSize={36}
+        />
+      </div>
+
+      {/* <DecryptedText text="COMING SOON!"/> */}
+
+      {/* <div className="text-5xl">
+        <DecryptedText
+          text="COMING SOON!"
+          animateOn="view"
+          revealDirection="center"
+          maxIterations={20}
+          speed={100}
+        />
+      </div> */}
     </div>
   );
 }
