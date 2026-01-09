da84baecce9d7ebe53df42421ae0d604a6a01e9c
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-01-09 19:48:59 +0530

    feat: added carausel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/landingpagecomponents/HeroSection";
+import HomeStays from "@/components/landingpagecomponents/HomeStays";
 import Navigation from "@/components/landingpagecomponents/Navigation";
 import SearchFilter from "@/components/landingpagecomponents/SearchFilter";
+import ToursAndTreks from "@/components/landingpagecomponents/TourAndTrek";
 
 export default function App() {
 	return (
@@ -13,6 +15,8 @@ export default function App() {
 			<div className="mt-[0.5]">
 				<SearchFilter />
 			</div>
+			<ToursAndTreks/>
+			<HomeStays/>
 		</div>
 	);
 }
